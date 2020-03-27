4000d77689d4fc8d9bd793e1a0c0ff66187666c3
andreacossio <andrea.cossio@studenti.polito.it>
2020-03-27 16:20:58 +0100

    Initial commit

---
+++ app/src/main/java/it/polito/mad/madmax/lab01/ShowProfileActivity.kt
@@ -0,0 +1,12 @@
+package it.polito.mad.madmax.lab01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.show_profile_activity)
+    }
+}
