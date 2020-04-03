f3dd3401b5c3f97c1414ec662680a18bfbd3d995
andreacossio <andrea.cossio@studenti.polito.it>
2020-04-03 17:30:50 +0200

    Changed user data field to be editable

--- app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
@@ -6,17 +6,15 @@ import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_show_profile.*
 
-
 class ShowProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        fullName.text = getString(R.string.fullName)
-        nickname.text = getString(R.string.nickname)
-        email.text = getString(R.string.email)
-        location.text = getString(R.string.location)
-
+        fullName.text = getString(R.string.name_default)
+        nickname.text = getString(R.string.nickname_default)
+        email.text = getString(R.string.email_default)
+        location.text = getString(R.string.location_default)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
