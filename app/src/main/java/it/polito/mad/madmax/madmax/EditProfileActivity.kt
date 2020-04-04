0d3eb1f144103e24c9c122208460768c89794e8e
andreacossio <andrea.cossio@studenti.polito.it>
2020-04-04 14:53:27 +0200

    Added save menu and persistent fields.

--- app/src/main/java/it/polito/mad/madmax/madmax/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/madmax/madmax/EditProfileActivity.kt
@@ -1,12 +1,39 @@
 package it.polito.mad.madmax.madmax
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class EditProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        //TODO get data from intent
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_save_profile, menu)
+        return true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("name", findViewById<TextInputEditText>(R.id.name_tiet).text.toString())
+        outState.putString("nickname", findViewById<TextInputEditText>(R.id.nickname_tiet).text.toString())
+        outState.putString("email", findViewById<TextInputEditText>(R.id.email_tiet).text.toString())
+        outState.putString("location", findViewById<TextInputEditText>(R.id.location_tiet).text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        findViewById<TextInputEditText>(R.id.name_tiet).setText(savedInstanceState.getString("name"))
+        findViewById<TextInputEditText>(R.id.nickname_tiet).setText(savedInstanceState.getString("nickname"))
+        findViewById<TextInputEditText>(R.id.email_tiet).setText(savedInstanceState.getString("email"))
+        findViewById<TextInputEditText>(R.id.location_tiet).setText(savedInstanceState.getString("location"))
     }
 }
--- app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_show_profile.*
 
 class ShowProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        fullName.text = getString(R.string.name_default)
-        nickname.text = getString(R.string.nickname_default)
-        email.text = getString(R.string.email_default)
-        location.text = getString(R.string.location_default)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
