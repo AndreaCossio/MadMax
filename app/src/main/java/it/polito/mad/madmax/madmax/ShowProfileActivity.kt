657e9e8d1f330650faefbb1434f7ad0fbab7927f
Daniele Cappuccio <s265554@studenti.polito.it>
2020-04-04 17:23:17 +0200

    Defined intent

--- app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
@@ -1,8 +1,10 @@
 package it.polito.mad.madmax.madmax
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ShowProfileActivity : AppCompatActivity() {
@@ -15,6 +17,16 @@ class ShowProfileActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_edit_profile, menu)
+        return editProfile()
+    }
+
+    private fun editProfile(): Boolean {
+        intent = Intent(applicationContext, EditProfileActivity::class.java)
+        intent.putExtra("it.polito.mad.madmax.madmax.name", findViewById<TextView>(R.id.name_tiet).toString())
+        intent.putExtra("it.polito.mad.madmax.madmax.nickname", findViewById<TextView>(R.id.nickname_tiet).toString())
+        intent.putExtra("it.polito.mad.madmax.madmax.email", findViewById<TextView>(R.id.email_tiet).toString())
+        intent.putExtra("it.polito.mad.madmax.madmax.location", findViewById<TextView>(R.id.location_tiet).toString())
+        startActivityForResult(intent, 1)
         return true
     }
 }
