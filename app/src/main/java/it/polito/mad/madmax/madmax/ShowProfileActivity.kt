a17a049edca1816b425c198c8101ab97069bbdb3
Dario Piazza <dario@ioma.ai>
2020-04-06 16:55:23 +0200

    added constants in res/values/strings and SharedPreferences in ShowProfile

--- app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/madmax/madmax/ShowProfileActivity.kt
@@ -1,34 +1,80 @@
 package it.polito.mad.madmax.madmax
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
-import android.widget.TextView
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_show_profile.*
 
 class ShowProfileActivity : AppCompatActivity() {
 
     private val INTENT_REQUEST_CODE = 1
 
+    var user:User? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+        val prefs = getSharedPreferences(getString(R.string.preference_file_key),Context.MODE_PRIVATE)
+        val profile  = prefs.getString("profile",null);
+        user = if(profile != null){
+            Gson().fromJson(profile,User::class.java)
+        }else{
+            User(
+                getString(R.string.name_default),
+                getString(R.string.nickname_default),
+                getString(R.string.email_default),
+                getString(R.string.location_default)
+            )
+        }
+
+        name_tv.text = user?.fullName
+        nickname_tv.text = user?.nickname
+        email_tv.text = user?.email
+        location_tv.text = user?.location
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu_edit_profile, menu)
-        return editProfile()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+             R.id.edit_profile -> {
+                editProfile()
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+
     }
 
     private fun editProfile(): Boolean {
         intent = Intent(applicationContext, EditProfileActivity::class.java)
-        intent.putExtra("it.polito.mad.madmax.madmax.name", findViewById<TextView>(R.id.name_tiet).toString())
-        intent.putExtra("it.polito.mad.madmax.madmax.nickname", findViewById<TextView>(R.id.nickname_tiet).toString())
-        intent.putExtra("it.polito.mad.madmax.madmax.email", findViewById<TextView>(R.id.email_tiet).toString())
-        intent.putExtra("it.polito.mad.madmax.madmax.location", findViewById<TextView>(R.id.location_tiet).toString())
+        intent.putExtra(getString(R.string.edited_name), user?.fullName)
+        intent.putExtra(getString(R.string.edited_nickname), user?.nickname)
+        intent.putExtra(getString(R.string.edited_email), user?.email)
+        intent.putExtra(getString(R.string.edited_location), user?.location)
         startActivityForResult(intent, INTENT_REQUEST_CODE)
         return true
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==INTENT_REQUEST_CODE && resultCode== Activity.RESULT_OK){
+            name_tv.text = data?.getStringExtra(getString(R.string.edited_name))
+            nickname_tv.text = data?.getStringExtra(getString(R.string.edited_nickname))
+            email_tv.text = data?.getStringExtra(getString(R.string.edited_email))
+            location_tv.text = data?.getStringExtra(getString(R.string.edited_location))
+        }
+    }
+
 }
