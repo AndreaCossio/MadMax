4da0ee3573db4008a722e99a4e65ae3603803b25
andreacossio <andrea.cossio@studenti.polito.it>
2020-04-17 17:05:58 +0200

    Lab2 Initial Commit

---
+++ app/src/main/java/it/polito/mad/madmax/lab02/MainActivity.kt
@@ -0,0 +1,12 @@
+package it.polito.mad.madmax.lab02
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
