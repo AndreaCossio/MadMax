62a56b2ebad826492207ccfef4dd41d0e04a6801
Daniele Cappuccio <s265554@studenti.polito.it>
2020-04-25 18:15:30 +0200

    Adjusted dependencies

--- app/src/main/java/it/polito/mad/madmax/lab02/ui/itemdetails/ItemListFragment.kt
+++ app/src/main/java/it/polito/mad/madmax/lab02/ui/itemdetails/ItemListFragment.kt
@@ -1,2 +1,35 @@
 package it.polito.mad.madmax.lab02.ui.itemdetails
 
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import it.polito.mad.madmax.lab02.*
+import it.polito.mad.madmax.lab02.data_models.Item
+import kotlinx.android.synthetic.main.item_details_fragment.*
+import java.io.Serializable
+
+class ItemListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.item_list_fragment, container, false)
+    }
+
+
+
+}
--- app/src/main/java/it/polito/mad/madmax/lab02/ui/itemdetails/RvAdapter.kt
+++ app/src/main/java/it/polito/mad/madmax/lab02/ui/itemdetails/RvAdapter.kt
@@ -1,4 +1,39 @@
 package it.polito.mad.madmax.lab02.ui.itemdetails
 
-class RvAdapter {
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import it.polito.mad.madmax.lab02.*
+import it.polito.mad.madmax.lab02.data_models.Item
+import kotlinx.android.synthetic.main.item_details_fragment.*
+import java.io.Serializable
+
+class RvAdapter(val items: ArrayList<Item>) : RecyclerView.Adapter<RvAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val v = LayoutInflater.from(p0?.context).inflate(R.layout.adapter_item_layout, p0, false)
+        return ViewHolder(v);
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        p0.title?.text = items[p1].title
+        p0.description?.text = items[p1].description
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title = itemView.findViewById<TextView>(R.id.title)
+        val description = itemView.findViewById<TextView>(R.id.description)
+    }
 }
