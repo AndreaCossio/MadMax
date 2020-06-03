c983544e2a70ab2ecc6c0f0cfe88a6fca9a0ddbb
andreacossio <andrea.cossio@studenti.polito.it>
2020-06-03 17:44:06 +0200

    Live position changes in showProfile

--- app/src/main/java/it/polito/mad/madmax/ui/profile/ShowProfileFragment.kt
+++ app/src/main/java/it/polito/mad/madmax/ui/profile/ShowProfileFragment.kt
@@ -8,7 +8,6 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -16,7 +15,6 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.firestore.ListenerRegistration
@@ -26,20 +24,18 @@ import it.polito.mad.madmax.*
 import it.polito.mad.madmax.data.model.User
 import it.polito.mad.madmax.data.viewmodel.UserViewModel
 import it.polito.mad.madmax.ui.MapsFragment
-import it.polito.mad.madmax.ui.item.OnSaleListFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_edit_profile.*
 import kotlinx.android.synthetic.main.fragment_show_profile.*
 import java.util.*
 
-class ShowProfileFragment : Fragment(),OnMapReadyCallback {
+class ShowProfileFragment : Fragment(), OnMapReadyCallback {
 
     // User
     private val userVM: UserViewModel by activityViewModels()
     private lateinit var userListener: ListenerRegistration
-    private lateinit var googleMap:GoogleMap
-    private var position: LatLng? = null
-    private lateinit var positionName:String
+
+    // Google map
+    private var gMap: GoogleMap? = null
 
     // Destination arguments
     private val args: ShowProfileFragmentArgs by navArgs()
@@ -50,10 +46,9 @@ class ShowProfileFragment : Fragment(),OnMapReadyCallback {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val layout =  inflater.inflate(R.layout.fragment_show_profile, container, false)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.profile_map_view) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-        return layout
+        return inflater.inflate(R.layout.fragment_show_profile, container, false).also {
+            (childFragmentManager.findFragmentById(R.id.profile_map_view) as SupportMapFragment).getMapAsync(this)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -69,57 +64,33 @@ class ShowProfileFragment : Fragment(),OnMapReadyCallback {
         // Card radius
         profile_card.addOnLayoutChangeListener(cardRadiusConstrain)
 
+        // Listen user data
         args.user?.also { userId ->
+            // Other's profile
             (requireActivity() as MainActivity).removeTopLevelProfile()
             userVM.getOtherUserData().observe(viewLifecycleOwner, Observer { user ->
                 if (user.userId != "") {
                     requireActivity().main_toolbar.title = user.name.split(" ")[0] + "'s Profile"
                     updateFields(user)
-
-                    getUserLocation(user.location)
-
-                    /*profile_show_location_map.setOnClickListener {
-                        val mapsDialog = MapsFragment().apply {
-                            setStyle(DialogFragment.STYLE_NORMAL, R.style.Theme_MadMax_Dialog)
-                            val locationArg = bundleOf("locationArg" to user.location)
-                            arguments = locationArg
-                        }
-                        mapsDialog.show(childFragmentManager,"")
-                    }*/
-
                 }
             })
             userListener = userVM.listenOtherUser(userId)
         } ?: run {
+            // My profile
             userVM.getCurrentUserData().observe(viewLifecycleOwner, Observer {
                 updateFields(it)
-                getUserLocation(it.location)
             })
         }
-
-
-
-        setFragmentResultListener("MAP_ADDRESS") { _, bundle ->
-            // We use a String here, but any type that can be put in a Bundle is supported
-            val result = bundle.getString("address")
-            profile_edit_location.setText(result)
-        }
-    }
-
-    private fun getUserLocation(uLocation:String){
-        positionName = uLocation
-        val address = Geocoder(requireContext(), Locale.getDefault()).getFromLocationName(uLocation,1)[0]
-        position  = LatLng(address.latitude,address.longitude)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        // Stop listening to other user data
-        if (this::userListener.isInitialized) {
+        args.user?.also {
+            // Stop listening to other user data
             userListener.remove()
+            // Restore top level destination
+            (requireActivity() as MainActivity).addTopLevelProfile()
         }
-        // Restore top level destination
-        args.user?.also { (requireActivity() as MainActivity).addTopLevelProfile() }
         // Detach layout listener
         profile_card.removeOnLayoutChangeListener(cardRadiusConstrain)
     }
@@ -146,18 +117,34 @@ class ShowProfileFragment : Fragment(),OnMapReadyCallback {
         }
     }
 
+    override fun onMapReady(p0: GoogleMap?) {
+        gMap = p0
+        args.user?.also {
+            userVM.getOtherUserData().value?.location?.also {
+                updateMarker(it)
+            } ?: gMap?.clear()
+        } ?: run {
+            userVM.getCurrentUserData().value?.location?.also {
+                updateMarker(it)
+            } ?: gMap?.clear()
+        }
+    }
+
     // Update views using the local variable user
     private fun updateFields(user: User) {
         profile_name.text = user.name
         profile_nickname.text = user.nickname
         profile_email.text = user.email
-        //profile_location.text = user.location
-        profile_phone.text = user.phone
 
         // Hide some fields for privacy
         args.user?.also {
             profile_phone.visibility = View.GONE
+        } ?: run {
+            profile_phone.text = user.phone
         }
+
+        // Location
+        updateMarker(user.location)
 
         // Profile photo
         profile_photo.post {
@@ -186,30 +173,33 @@ class ShowProfileFragment : Fragment(),OnMapReadyCallback {
         }
     }
 
+    private fun updateMarker(location: String) {
+        if (location != "") {
+            val address = Geocoder(requireContext(), Locale.getDefault()).getFromLocationName(location, 1)[0]
+            val position = LatLng(address.latitude, address.longitude)
+            gMap?.apply {
+                clear()
+                addMarker(MarkerOptions().position(position))
+                animateCamera(CameraUpdateFactory.newLatLngZoom(position, 13.5F))
+                uiSettings.isZoomControlsEnabled = true
+                args.user?.also {
+                    setOnMapClickListener {
+                        val mapsDialog = MapsFragment().apply {
+                            setStyle(DialogFragment.STYLE_NORMAL, R.style.Theme_MadMax_Dialog)
+                            val locationArg = bundleOf("locationArg" to location)
+                            arguments = locationArg
+                        }
+                        mapsDialog.show(childFragmentManager, "")
+                    }
+                }
+            }
+        } else {
+            gMap?.clear()
+        }
+    }
+
     // Companion
     companion object {
         const val TAG = "MM_SHOW_PROFILE"
-    }
-
-    override fun onMapReady(p0: GoogleMap?) {
-
-        googleMap = p0!!
-        if(position != null){
-            googleMap.apply {
-                addMarker(MarkerOptions().position(position!!))
-                animateCamera(CameraUpdateFactory.newLatLngZoom(position, 13.5F))
-                uiSettings.isZoomControlsEnabled = true
-                setOnMapClickListener {
-                    val mapsDialog = MapsFragment().apply {
-                        setStyle(DialogFragment.STYLE_NORMAL, R.style.Theme_MadMax_Dialog)
-                        val locationArg = bundleOf("locationArg" to positionName)
-                        arguments = locationArg
-                    }
-                    mapsDialog.show(childFragmentManager,"")
-
-                }
-            }
-        }
-
     }
 }
