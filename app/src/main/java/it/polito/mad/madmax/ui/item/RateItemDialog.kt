8f6112781cb2a2c99920b007d8de45e17704af3e
Daniele Cappuccio <s265554@studenti.polito.it>
2020-06-05 17:36:54 +0200

    rateItemDialog finalization

--- app/src/main/java/it/polito/mad/madmax/ui/item/RateItemDialog.kt
+++ app/src/main/java/it/polito/mad/madmax/ui/item/RateItemDialog.kt
@@ -5,23 +5,31 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
 import it.polito.mad.madmax.R
+import it.polito.mad.madmax.data.model.Item
+import it.polito.mad.madmax.data.viewmodel.ItemViewModel
 
 class RateItemDialog : DialogFragment() {
+
+    private val itemVM: ItemViewModel by activityViewModels()
+    private lateinit var item: Item
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        item = itemVM.getSingleItem().value!!
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            // Get the layout inflater
-            val inflater = requireActivity().layoutInflater;
+            val inflater = requireActivity().layoutInflater
 
-            // Inflate and set the layout for the dialog
-            // Pass null as the parent view because its going in the dialog layout
             builder.setView(inflater.inflate(R.layout.rate_dialog, null))
-                // Add action buttons
                 .setPositiveButton("Rate",
                     DialogInterface.OnClickListener { dialog, id ->
-                        // TODO: apply rating
+                        val rating: Float = R.id.ratingBar.toFloat()
+                        itemVM.rateUser(item, rating)
                     })
                 .setNegativeButton("Cancel",
                     DialogInterface.OnClickListener { dialog, id ->
